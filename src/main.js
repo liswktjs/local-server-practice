13053a97b1485c60836b3d4a55473dda694f629b
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-11-07 18:57:05 +0900

    ✨ api 라우팅 처리 & test

--- src/main.js
+++ src/main.js
@@ -1,1 +1,26 @@
 //@ts-check
+const http = require('http')
+
+const server = http.createServer((req, res) => {
+  if (req.url === '/posts' && req.method === 'GET') {
+    res.statusCode = 200
+    res.end('list of post')
+  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+    res.statusCode = 200
+    res.end('Some Content of the post')
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    res.statusCode = 200
+    res.end('creating post')
+  } else {
+    res.statusCode = 404
+    res.end('Not Found')
+  }
+  res.statusCode = 200
+  res.end('Hell0')
+})
+
+const PORT = 4000
+
+server.listen(PORT, () => {
+  console.log('This server is work!')
+})

b7f107064561ce3064f20436ac74985c74a390b6
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-11-08 11:42:17 +0900

    정규식 활용한 url에서 id 값 추출하기

--- src/main.js
+++ src/main.js
@@ -2,12 +2,20 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
+  const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
+  const postIdRegaxResult =
+    (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
+
   if (req.url === '/posts' && req.method === 'GET') {
     res.statusCode = 200
     res.end('list of post')
-  } else if (req.url && /^\/posts\/[a-zA-Z0-9-_]+$/.test(req.url)) {
+  } else if (postIdRegaxResult) {
+    // GET /posts/lid
+    const postId = postIdRegaxResult[1]
+    console.log(postId)
+
     res.statusCode = 200
-    res.end('Some Content of the post')
+    res.end(`${postId} content of the post`)
   } else if (req.url === '/posts' && req.method === 'POST') {
     res.statusCode = 200
     res.end('creating post')

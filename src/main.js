c2827c68d60bc518b8c826cc6392825357455092
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-11-08 12:59:34 +0900

    api code

--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ const posts = [
   },
   {
     id: 'my_second_post',
-    title: 'My second post',
+    title: '나의 두번째 포스트',
     content: 'hello world2',
   },
 ]
@@ -28,16 +28,45 @@ const server = http.createServer((req, res) => {
     (req.url && POSTS_ID_REGEX.exec(req.url)) || undefined
 
   if (req.url === '/posts' && req.method === 'GET') {
+    const result = {
+      posts: posts.map((post) => ({
+        id: post.id,
+        title: post.title,
+      })),
+      totalCount: posts.length,
+    }
     res.statusCode = 200
-    res.end('list of post')
-  } else if (postIdRegaxResult) {
+    res.setHeader('Content-Type', 'application/json; charset=utf-8')
+    res.end(JSON.stringify(result))
+  } else if (postIdRegaxResult && req.method == 'GET') {
     // GET /posts/lid
     const postId = postIdRegaxResult[1]
-    console.log(postId)
 
-    res.statusCode = 200
-    res.end(`${postId} content of the post`)
+    const post = posts.find((_post) => _post.id === postId)
+    if (post) {
+      res.statusCode = 200
+      res.setHeader('Content-Type', 'application/json; charset=utf-8')
+      res.end(JSON.stringify(post))
+    } else {
+      res.statusCode = 404
+      res.end('Post not Found')
+    }
   } else if (req.url === '/posts' && req.method === 'POST') {
+    req.setEncoding('utf-8')
+    req.on('data', (data) => {
+      /**
+       * @typedef CreatePostBody
+       * @property {string} title
+       * @property {string} content
+       */
+      /**@type {CreatePostBody} */
+      const body = JSON.parse(data)
+      posts.push({
+        id: body.title.toLowerCase().replace(/\s/g, '_'),
+        title: body.title,
+        content: body.content,
+      })
+    })
     res.statusCode = 200
     res.end('creating post')
   } else {

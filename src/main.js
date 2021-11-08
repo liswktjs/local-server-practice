61e395a80c0058156b8d80315a026a9a9d363905
liswktjs <60773373+liswktjs@users.noreply.github.com>
2021-11-08 11:56:23 +0900

    JSDoc 활용한 데이터 타입확인하기

--- src/main.js
+++ src/main.js
@@ -1,6 +1,27 @@
 //@ts-check
 const http = require('http')
 
+/**
+ * @typedef  Post
+ * @property {string} id
+ * @property {string} title
+ * @property {string} content
+ */
+
+/** @type {Post[]} */
+const posts = [
+  {
+    id: 'my_first_post',
+    title: 'My frist post',
+    content: 'hello world',
+  },
+  {
+    id: 'my_second_post',
+    title: 'My second post',
+    content: 'hello world2',
+  },
+]
+
 const server = http.createServer((req, res) => {
   const POSTS_ID_REGEX = /^\/posts\/([a-zA-Z0-9-_]+)$/
   const postIdRegaxResult =
